ddd896d247b9110ea7a92730abd46d5db74d4930
codebyharish <127782734+codebyharish@users.noreply.github.com>
2026-03-09 11:38:42 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,29 @@
+package com.example;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    App calc = new App();
+
+    @Test
+    public void testAdd() {
+        assertEquals(15, calc.add(10,5));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(5, calc.subtract(10,5));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(50, calc.multiply(10,5));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0, calc.divide(10,5), 0.01);
+    }
+}
